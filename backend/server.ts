518faf2e8409e7e700b4c477edab70b2a1dae84c
Hanna Rogalska <hanrogalska@gmail.com>
2026-05-22 11:47:03 +0200

    chore(server): initialize express application and typescript configuration

---
+++ backend/server.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import connectDB from './config/db';
+import app from './app'
+
+
+
+const PORT: string | number = process.env.PORT || 3001;
+
+const startServer = async (): Promise<void> => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Critical server startup error:', error);
+    process.exit(1);
+  }
+};
+startServer();
