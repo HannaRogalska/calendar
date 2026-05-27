258d5f46dd7a2806045fc225083a1e9dfbf10b83
Hanna Rogalska <hanrogalska@gmail.com>
2026-05-27 18:40:01 +0200

    move res.headersSent guard to the top off errorHandler

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -3,7 +3,13 @@ import { ZodError } from 'zod';
 import mongoose from 'mongoose';
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({
+    message: 'Internal server error',
+  });
+  console.error(`[⚠️ Server Error]: ${err.stack || err}`);
   if (err instanceof ZodError) {
     res.status(400).json({
       message: 'Validation failed',
@@ -18,11 +24,6 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     });
     return;
   }
-  console.error(`[⚠️ Server Error]: ${err.stack || err}`);
-  if (res.headersSent) {
-    return next(err)
-  }
-  res.status(500).json({
-    message: 'Internal server error',
-  });
+
+
 };
