4c219f974685772fb1660540c0595ebdc870db22
Hanna Rogalska <hanrogalska@gmail.com>
2026-06-12 13:07:49 +0200

    Configure Vercel fullstack deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,15 +4,13 @@ import app from './app';
 
 const PORT: string | number = process.env.PORT || 3001;
 
-const startServer = async (): Promise<void> => {
+const handler = async (req: any, res: any) => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
+    return app(req, res);
   } catch (error) {
-    console.error('Critical server startup error:', error);
-    process.exit(1);
+    res.status(500).json({ error: 'Critical server startup error:' });
   }
 };
-startServer();
+
+export default handler;
